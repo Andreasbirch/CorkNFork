2d1c26d45cb37a5297d1f2542cb6cede967f212a
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2020-06-15 17:53:17 +0200

    Tilføjet yderligere menuer til hovedfragmentet - Fiks at der laves flere instanser af fragment_home

--- app/src/main/java/com/example/andreas/cork/HomeFragment.java
+++ app/src/main/java/com/example/andreas/cork/HomeFragment.java
@@ -1,11 +1,15 @@
 package com.example.andreas.cork;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by andreas on 15/06/2020.
@@ -15,7 +19,34 @@ public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button gotoMealtypeSelect = (Button) view.findViewById(R.id.goto_mealtype_select);
+        Button gotoWinetypeSelect = (Button) view.findViewById(R.id.goto_winetype_select);
+
+        gotoMealtypeSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new ChooseMealtypeFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        gotoWinetypeSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new ChooseWinetypeFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        return view;
     }
+
 }
