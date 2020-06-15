849616d23abb13631ce3ab4a3b672d7dd872d594
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2020-06-15 15:39:40 +0200

    Opdateret layout, ændret i opsætningen

--- app/src/main/java/com/example/andreas/cork/HomeFragment.java
+++ app/src/main/java/com/example/andreas/cork/HomeFragment.java
@@ -1,21 +1,21 @@
 package com.example.andreas.cork;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * Created by andreas on 11/06/2020.
+ * Created by andreas on 15/06/2020.
  */
 
 public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
--- app/src/main/java/com/example/andreas/cork/MainActivity.java
+++ app/src/main/java/com/example/andreas/cork/MainActivity.java
@@ -3,48 +3,43 @@ package com.example.andreas.cork;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
-
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    //mTextMessage.setText(R.string.title_home);
-                    selectedFragment = new HomeFragment();
-                    break;
-                case R.id.navigation_settings:
-                    selectedFragment = new HomeFragment();
-                    break;
-                case R.id.navigation_user:
-                    selectedFragment = new UploadFragment();
-                    break;
-            }
-            getFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
-            return true;
-        }
-
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
     }
 
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.navigation_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                        case R.id.navigation_user:
+                            selectedFragment = new UserFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+    };
 }
