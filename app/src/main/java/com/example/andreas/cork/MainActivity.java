f0971611284db25f350eb1bd2d3bff03ebd9b82a
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2020-06-15 13:26:35 +0200

    Init commit

---
+++ app/src/main/java/com/example/andreas/cork/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.andreas.cork;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //mTextMessage.setText(R.string.title_home);
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_settings:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_user:
+                    selectedFragment = new UploadFragment();
+                    break;
+            }
+            getFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
+            return true;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
