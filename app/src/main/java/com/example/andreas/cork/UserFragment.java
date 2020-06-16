cf40acd50a891daa28cb7f18439e944a8b09bdaf
cbfallesen <carolinefallesen@gmail.com>
2020-06-16 12:50:17 +0200

    User fragment

--- app/src/main/java/com/example/andreas/cork/UserFragment.java
+++ app/src/main/java/com/example/andreas/cork/UserFragment.java
@@ -3,9 +3,11 @@ package com.example.andreas.cork;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by andreas on 11/06/2020.
@@ -16,6 +18,23 @@ public class UserFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+
+        Button logOut = (Button) view.findViewById(R.id.logoutButton);
+        Button myFavorites = (Button) view.findViewById(R.id.myFavorites);
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new LogOutFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        return view;
     }
 }
