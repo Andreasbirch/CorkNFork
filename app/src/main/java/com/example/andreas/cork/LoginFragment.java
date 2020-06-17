5a0a44cb6a4273d12937a98393cb1748e395b4fb
maxx1559 <55869522+maxx1559@users.noreply.github.com>
2020-06-17 17:01:10 +0200

    Firebase authentication vi har brugere nu

--- app/src/main/java/com/example/andreas/cork/LoginFragment.java
+++ app/src/main/java/com/example/andreas/cork/LoginFragment.java
@@ -1,15 +1,24 @@
 package com.example.andreas.cork;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
@@ -18,13 +27,19 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginFragment extends Fragment {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
+    private static final String TAG = "EmailPassword";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+        mAuth = FirebaseAuth.getInstance();
 
         Button signUp = (Button) view.findViewById(R.id.signUp);
         Button login = (Button) view.findViewById(R.id.login);
+        final EditText email = view.findViewById(R.id.editTextTextEmailAddress);
+        final EditText password = view.findViewById(R.id.editTextTextPassword);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,15 +54,38 @@ public class LoginFragment extends Fragment {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, new UserFragment());
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                   Toast.makeText(getActivity().getApplicationContext(), "Authentication failed.",
+                                           Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+
+                            }
+                        });
+
             }
         });
 
         return view;
 
+    }
+
+    private void updateUI(FirebaseUser user) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new UserFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
 }
